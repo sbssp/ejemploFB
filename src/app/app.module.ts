5d77974c81f6ce12c9f742077bd87ad9f9ffc64c
sbssp <jsebastian8507@gmail.com>
2022-05-15 21:36:50 -0300

    primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
